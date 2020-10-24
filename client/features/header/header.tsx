009014cc1362518647e1070efb4018e4a5e4f7c2
RioDel <daniokipaky@mail.ru>
2020-10-25 02:27:22 +0300

    new-vacancy

--- client/features/header/header.tsx
+++ client/features/header/header.tsx
@@ -12,44 +12,6 @@ export function HeaderTop() {
         <i>SKFU</i>
       </Link>
       <Flex justifyContent="space-between" mr="50px">
-<<<<<<< HEAD
-        <NavLink href="/vacancies">
-          <Link href="#" m="0 20px" _focus={{ border: "none" }} position="relative" className="headerLink">
-            <Box
-              className="GreenHover"
-              zIndex={-1}
-              opacity="0"
-              position="absolute"
-              left="-80px"
-              top="-210px"
-              background="#27AE60"
-              boxShadow="box-shadow: inset 0px 0px 6px rgba(0, 0, 0, 0.25), inset 0px 0px 23px rgba(0, 0, 0, 0.2)"
-              borderRadius={30}
-              h={250}
-              w={250}
-            ></Box>
-            Вакансии
-          </Link>
-        </NavLink>
-        <NavLink href="/resumes">
-          <Link href="#" m="0 20px" _focus={{ border: "none" }} position="relative" className="headerLink">
-            <Box
-            className="GreenHover"
-              zIndex={-1}
-              position="absolute"
-              left="-80px"
-              opacity="0"
-              top="-210px"
-              background="#27AE60"
-              boxShadow="box-shadow: inset 0px 0px 6px rgba(0, 0, 0, 0.25), inset 0px 0px 23px rgba(0, 0, 0, 0.2)"
-              borderRadius={30}
-              h={250}
-              w={250}
-            ></Box>
-            Резюме
-          </Link>
-        </NavLink>
-=======
         <Link href="/vacancies" m="0 20px" _focus={{ border: "none" }} position="relative" className="headerLink">
           <Box
             className="GreenHover"
@@ -114,7 +76,6 @@ export function HeaderTop() {
           ></Box>
           Личный кабинет
         </Link>
->>>>>>> b1d2f750a82b3f1a4be87747166459c750b0228b
       </Flex>
       <NavLink href="/personal-account">
         <Image src="/Ellipse 1.svg" cursor="pointer" h="40px" w="40px" mr="50px"></Image>

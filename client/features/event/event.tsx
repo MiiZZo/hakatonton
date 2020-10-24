b398caa6aba49d9bca360bbf500216e22a5dbc0c
RioDel <daniokipaky@mail.ru>
2020-10-24 14:37:48 +0300

    event

--- client/features/event/event.tsx
+++ client/features/event/event.tsx
@@ -25,11 +25,11 @@ import {
 export function Event() {
   return (
     <>
-      <Box rounded={15} border="1px solid #E2E8F0" maxH={300} maxW={300}>
+      <Box rounded={15} border="1px solid #E2E8F0"  maxW={300}>
         <Box h={130} roundedTop={15} background="#f1f1f1"></Box>
         <Flex m="10px 24px">
           <Heading size="md">Title Paragraph</Heading>
-          <Text m="auto" mr="-4px">
+          <Text color="#BDBDBD" m="auto" mr="-4px">
             21.12.2112
           </Text>
         </Flex>
@@ -37,15 +37,16 @@ export function Event() {
           Lorem ipsum carrots, enhanced undergraduate developer, but they do
           occaecat time.
         </Text>
-        <Flex maxW="252px" m="0px 24px 18px">
-          <Tag mr="5px">IT</Tag>
-          <Tag textAlign="center">SQL</Tag>
-          <Text mt="6px" fontSize="14px">
+        <Flex maxW="252px" m="0 24px 18px" >
+          <Tag  mr="5px">IT</Tag>
+          <Tag   mr="5px">SQL</Tag>
+          <Tag minW="none" paddingLeft="0.5rem" textAlign="center"  fontSize="14px">
             +7
-          </Text>
+          </Tag>
           <Button
-            ml="42px"
+            ml="32px"
             h="30px"
+            fontSize="13px"
             background="none"
             border="2px solid #3182CE"
             color="#3182CE"
